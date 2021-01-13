5db24d9476c4f5eb946dc98d2bc1d0289beefb37
Cullen S <cullensmi@gmail.com>
2021-01-12 21:32:04 -0500

    Added v1, v2, v3 and v4

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -3,6 +3,24 @@ package com.csmithswim;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        //Version 1
+        System.out.println("Version 1");
+        ModuloTimeCalculator.convertHours(8,0,5);
+        ModuloTimeCalculator.convertHours(21,0,5);
+        ModuloTimeCalculator.convertHours(21,0,36);
+
+        //Version 2
+        System.out.println("Version 2");
+        ModuloTimeCalculator.convertHours(21,0,-5);
+
+        //Version 3
+        System.out.println("Version 3");
+        ModuloTimeCalculator.convertMinutes(21,00,147);
+
+        //Version 4
+        System.out.println("Version 4");
+        ModuloTimeCalculator.convertHours(21,27,2,false);
+        ModuloTimeCalculator.convertMinutes(21,0,147,true);
+
     }
 }
--- src/com/csmithswim/ModuloTimeCalculator.java
+++ src/com/csmithswim/ModuloTimeCalculator.java
@@ -1,4 +1,33 @@
 package com.csmithswim;
 
 public class ModuloTimeCalculator {
+    //Version 1
+    static void convertHours(int hours, int minutes, int hoursPassed){
+        int finalHours = (hours+(hoursPassed%24))%24;
+        System.out.println(finalHours+":"+minutes);
+    }
+
+    static void convertMinutes(int hours, int minutes, int minutesPassed){
+        int finalHours = hours + minutes + (minutesPassed/60);
+        int finalMinutes = (minutesPassed%60);
+        System.out.println(finalHours+":"+finalMinutes);
+    }
+
+    static void convertHours(int hours, int minutes, int hoursPassed, boolean standard){
+        int finalHours = (hours+(hoursPassed%24))%24;
+        if (standard==true){
+            System.out.println(finalHours%12+":"+minutes);
+        }
+        else System.out.println(finalHours+":"+minutes);
+    }
+
+    static void convertMinutes(int hours, int minutes, int minutesPassed, boolean standard){
+        int finalHours = hours  + (minutesPassed/60);
+        int finalMinutes = (minutesPassed+minutes)%60;
+        if (standard==true){
+            System.out.println(finalHours%12+":"+finalMinutes);
+        }
+        else System.out.println(finalHours+":"+finalMinutes);
+    }
+
 }
